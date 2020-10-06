2b26096e8fc296ab451bd2b1001026df91a01c87
Akshat Zala <akshatzala@gmail.com>
2020-10-06 17:28:20 +0530

    Added space to text

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Lifecycle from './lifecycle.component'
 
-function App() {
+class App extends React.Component {
+  constructor(){
+    super();
+  
+    this.state={
+      showChild: true,
+      text: ''
+    };
+  }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  render(){
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <button
+            onClick={() =>
+              this.setState(state => ({
+                showChild: !state.showChild
+              }))
+            }
+          >
+            Toggle Lifecycles
+          </button>
+          <button
+            onClick={() =>
+              this.setState(state => ({
+                text: '_hello ' + state.text
+              }))
+            }
+          >
+            Update Text
+          </button>
+          {this.state.showChild ? <Lifecycle text={this.state.text} /> : null}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
